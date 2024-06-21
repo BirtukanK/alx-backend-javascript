ec2bd3013ab3cd837a7bc54140d9186fd9089774
BirtukanK <birtukankuma1113@gmail.com>
2024-06-21 08:06:16 +0000

    using stub

---
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -0,0 +1,17 @@
+const sinon = require('sinon');
+const Utils = require('./utils');
+const { expect } = require('chai');
+const sendPaymentRequestToApi = require('./3-payment');
+
+describe('sendPaymentRequestToApi', () => {
+  it('sendPaymentRequestToApi uses the calculateNumber method of Utils', () => {
+    const stub = sinon.stub().throws();
+    const spy = sinon.spy(Utils);
+
+    sendPaymentRequestToApi(100, 20);
+    expect(spy.calculateNumber.calledWith('SUM', 100, 20)).to.be.true;
+    expect(spy.calculateNumber.callCount).to.be.equal(10);
+    expect(stub.calculateNumber.calledBefore(spy1));
+    spy.calculateNumber.restore();
+  });
+});
