d4f466faf5525accd89a876e198acda38dfe5070
BirtukanK <birtukankuma1113@gmail.com>
2024-06-21 08:21:06 +0000

    using stub

--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -5,13 +5,16 @@ const sendPaymentRequestToApi = require('./3-payment');
 
 describe('sendPaymentRequestToApi', () => {
   it('sendPaymentRequestToApi uses the calculateNumber method of Utils', () => {
-    const stub = sinon.stub().throws();
+    const stub = sinon.stub(Utils, 'calculateNumber');
     const spy = sinon.spy(Utils);
 
+    stub.returns(10);
     sendPaymentRequestToApi(100, 20);
-    expect(spy.calculateNumber.calledWith('SUM', 100, 20)).to.be.true;
-    expect(spy.calculateNumber.callCount).to.be.equal(10);
-    expect(stub.calculateNumber.calledBefore(spy1));
-    spy.calculateNumber.restore();
+    expect(stub.calledWith('SUM', 100, 20)).to.be.true;
+    expect(stub.callCount).to.be.equal(1);
+    expect(spy.calculateNumber.calledWith('The total is: 10')).to.be.true;
+    expect(spy.calculateNumber.callCount).to.be.equal(1);
+    stub.restore()
+    spy.log.restore();
   });
 });
